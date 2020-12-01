692e882bd0e3d31d3dcd5e0786e81b18e09585c4
enr109 <56203491+enr109@users.noreply.github.com>
2020-11-30 18:50:40 -0600

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="src/css/estilo.css">
+    <link rel="stylesheet" href="src/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="../src/css/bootstrap.css">
+    <script src="../src/js/jquery-3.4.1.js"></script>
+    <script src="../src/js/bootstrap.js"></script>
+    <script src="../src/js/extras.js"></script>-->
+</head>
+
+<body>
+  <?php
+  require("header2.html");
+  ?>
+  <br>
+  <div class="container">
+  <main role="main" class="container">
+
+ <div class="row">
+
+   <div class="col-12 col-md-12">
+
+     <p class="float-right d-md-none">
+       <button type="button" class="btn btn-primary btn-sm" data-toggle="offcanvas">Toggle nav</button>
+     </p>
+
+     <div class="gmcont">
+       <div class="embed-responsive embed-responsive-16by9">
+
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29770.135210722623!2d-98.4308479067574!3d21.14177404996643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d726937c6d4e45%3A0x4943316bd6808859!2sHuejutla%2C%20Hgo.!5e0!3m2!1ses-419!2smx!4v1603771262267!5m2!1ses-419!2smx" frameborder="0" style="border:0" allowfullscreen></iframe>
+       </div>
+     </div>
+
+   </div><!--/span-->
+
+
+ </div><!--/row-->
+
+ <hr>
+
+ <div class="row">
+
+   <div class="col-12 col-md-5">
+
+     <h4 class="font-italic">Datos</h4>
+
+     <div class="direcont">
+
+       <i class="fas fa-map-marked-alt"></i> Huejutla de Reyes, Hidalgo. 4300.
+       <br>
+       <i class="fas fa-phone"></i> 01 111-1111
+       <br>
+       <i class="fas fa-envelope-square"></i> mail@mail.com
+
+
+       <br>
+       <br>
+       <br>
+       <br>
+
+     </div>
+
+   </div><!--/span-->
+
+
+ </div><!--/row-->
+
+</main>
+
+  </div>
+  <?php
+  require('footer.html');
+  ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
